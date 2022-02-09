eca9d8466f91095f38df332afca830f8e65939c3
Frederick Lee <frederick.lee@cognizant.com>
2022-02-09 12:55:31 -0800

    Initial Commit

---
+++ RicTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RicTableView
+//
+//  Created by Frederick Lee on 2/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
