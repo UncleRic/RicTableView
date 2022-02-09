85f692056b93ef9693707a7130718a384af772d4
Frederick Lee <frederick.lee@cognizant.com>
2022-02-09 13:25:05 -0800

    Working Demo.

---
+++ RicTableView/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  RicTableView
+//
+//  Created by Frederick Lee on 2/9/22.
+//
+
+import UIKit
+
+class RicCell: UITableViewCell {}
+
+class MainViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(RicCell.self, forCellReuseIdentifier: "RicCell")
+        data = ["Larry", "Moe", "Curly"]
+    }
+    
+    @IBAction func exitAction(_ sender: Any) {
+        exit(0)
+    }
+    
+    // ----------------------------------------------------------------------------------------------
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RicCell", for: indexPath) as! RicCell
+        cell.textLabel!.text = data[indexPath.row]
+        return cell
+    }
+    
+}
+
